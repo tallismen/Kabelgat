9462b14362565b09675c33d6604ab3ad56234e2a
tallismen <tallismen@gmail.com>
2020-12-10 19:49:13 +0100

    Added bottom nav menu

--- app/src/main/java/nl/designlama/kabelgat/ui/main/MainFragment.kt
+++ app/src/main/java/nl/designlama/kabelgat/ui/main/MainFragment.kt
@@ -10,23 +10,10 @@ import nl.designlama.kabelgat.R
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     val viewModel: MainViewModel by activityViewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
 }
