c67fe2e34fe0898b69e24cb1aea3c8eb6c3c510c
tallismen <tallismen@gmail.com>
2020-12-10 18:34:28 +0100

    Initial commit

---
+++ app/src/main/java/nl/designlama/kabelgat/MainActivity.kt
@@ -0,0 +1,18 @@
+package nl.designlama.kabelgat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nl.designlama.kabelgat.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/nl/designlama/kabelgat/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package nl.designlama.kabelgat.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
